316b279a062ed948e275a16ce30c9c38337bf048
azzyjk <56161518+azzyjk@users.noreply.github.com>
2021-02-17 23:20:03 +0900

    RN[17] : Change ChatScreen file

--- frontend/Screen/ChatScreen.js
+++ frontend/Screen/ChatScreen.js
@@ -1,238 +1,234 @@
-import React from 'react'
-import { Component } from 'react';
-import { View, Text,Dimensions } from 'react-native';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
-import axios from 'axios';
+import React from "react";
+import { Component } from "react";
+import { View, Text, Dimensions } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-const {height, width} = Dimensions.get("window")
+const { height, width } = Dimensions.get("window");
 
 class ChatScreen extends Component {
-    constructor(props){
-        super(props);
-        this.entranceYear = AsyncStorage.getItem("entranceYear");
-        this.department = AsyncStorage.getItem("department");
-        //this.entranceYear = this.props.route.params.entranceYear;
-        //this.department = this.props.route.params.department;
-        if(this.department){
-            if(this.department == '컴퓨터공학부'){
-                this.departmentId = 'c';
-            }
-            else if(this.department == '전기전자공학부'){
-                this.departmentId = 'e';
-            }
-            else if(this.department == '기계항공공학부'){
-                this.departmentId = 'm';
-            }
-            else{
-                this.departmentId = '';
-            }
-            this.departmentId = this.departmentId + this.entranceYear[2] + this.entranceYear[3]
-;        }
-        
-        this.state = {
-            log : [],
-            msg : "",
-            count : 1000,
+  constructor(props) {
+    super(props);
+    AsyncStorage.getItem("entranceYear", (err, result) => {
+      this.entranceYear = result;
+      AsyncStorage.getItem("department", (err, result) => {
+        this.department = result;
+
+        if (this.department) {
+          if (this.department == "컴퓨터공학부") {
+            this.departmentId = "c";
+          } else if (this.department == "전기전자공학부") {
+            this.departmentId = "e";
+          } else if (this.department == "기계항공공학부") {
+            this.departmentId = "m";
+          } else {
+            this.departmentId = "";
+          }
+          this.departmentId =
+            this.departmentId + this.entranceYear[2] + this.entranceYear[3];
+        }
+      });
+    });
+    this.state = {
+      log: [],
+      msg: "",
+      count: 1000,
+    };
+  }
+
+  componentDidMount() {
+    var danbee_head = {
+      "Content-Type": "application/json;charset=UTF-8",
+    };
+
+    var danbee_body = {
+      user_id: "cse17",
+    };
+
+    var danbee_url =
+      "https://danbee.ai/chatflow/chatbot/v2.0/1d1e0598-3c44-4a8e-bd41-908a9c14846a/welcome.do";
+
+    var options = {
+      headers: danbee_head,
+      data: JSON.stringify(danbee_body),
+      method: "POST",
+      url: danbee_url,
+    };
+
+    axios(options).then((res) => {
+      var preLog = [];
+      var tmpMessage = {
+        _id: this.state.count,
+        text: res.data.responseSet.result.result[0].message,
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar:
+            "https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg",
+        },
+      };
+      preLog = [...preLog, tmpMessage];
+      this.setState({
+        log: preLog,
+        msg: tmpMessage,
+        count: this.state.count + 1,
+        tmp: 0,
+      });
+
+      //console.log(this.entranceYear);
+    });
+  }
+
+  renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        textStyle={{
+          right: {
+            color: "white",
+          },
+          left: {
+            color: "black",
+          },
+        }}
+        wrapperStyle={{
+          right: {
+            backgroundColor: "#99CCFF",
+          },
+          left: {
+            backgroundColor: "#CCFFCC",
+          },
+        }}
+      />
+    );
+  };
+
+  render() {
+    const onSend = (messages) => {
+      var danbee_head = {
+        "Content-Type": "application/json;charset=UTF-8",
+      };
+
+      var danbee_body = {
+        input_sentence: messages[0].text,
+        user_id: this.departmentId,
+      };
+
+      var danbee_url =
+        "https://danbee.ai/chatflow/chatbot/v2.0/1d1e0598-3c44-4a8e-bd41-908a9c14846a/message.do";
+
+      var options = {
+        headers: danbee_head,
+        data: JSON.stringify(danbee_body),
+        method: "POST",
+        url: danbee_url,
+      };
+
+      axios(options).then((res) => {
+        var preLog = this.state.log;
+
+        var cnt = this.state.count + 1;
+        var replyMsg = res.data.responseSet.result.result[0].message;
+
+        var reply;
+        var send;
+
+        reply = {
+          _id: cnt,
+          text: replyMsg,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar:
+              "https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg",
+          },
         };
-    }
-    
+        preLog = [messages[0], ...preLog];
+        preLog = [reply, ...preLog];
 
-    componentDidMount(){
-        var danbee_head = {
-            'Content-Type' : 'application/json;charset=UTF-8'
-        }
-        
-        var danbee_body = {
-            "user_id" : 'cse17',
-        }
-        
-        var danbee_url = "https://danbee.ai/chatflow/chatbot/v2.0/1d1e0598-3c44-4a8e-bd41-908a9c14846a/welcome.do";
-
-        var options = {
-            headers : danbee_head,
-            data : JSON.stringify(danbee_body),
-            method : "POST",
-            url : danbee_url
-        };
-
-        axios(options).then((res) =>{
-
-            var preLog = [];
-            var tmpMessage = {
-                _id: this.state.count,
-                text: res.data.responseSet.result.result[0].message,
-                createdAt: new Date(),
-                user: {
-                    _id: 2,
-                    name: 'React Native',
-                    avatar: 'https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg',
-                    
-                }
-            }
-            preLog = [...preLog,tmpMessage];
-            this.setState({
-                log : preLog,
-                msg : tmpMessage,
-                count : this.state.count + 1,
-                tmp : 0
-            });
-
-            console.log(this.department + this.entranceYear);
-        });
-        
-        
-        
-    }
-
-    renderBubble  =(props) => {
-        return (
-          <Bubble
-            {...props}
-            textStyle={{
-              right: {
-                color: "white"
-              },
-              left: {
-                color: "black"
-              }
-            }}
-            wrapperStyle={{
-                right: {
-                  backgroundColor: "#99CCFF"
-                },
-                left : {
-                    backgroundColor : "#CCFFCC"
-                }
-              }}
-          />
-        )
-      }
-   
-    render(){
-        
-
-        const onSend = (messages) =>{
-            
-
-            var danbee_head = {
-                'Content-Type' : 'application/json;charset=UTF-8'
-            }
-            
-            var danbee_body = {
-                "input_sentence" : messages[0].text,
-                "user_id" : this.departmentId,
-            }
-            
-            var danbee_url = "https://danbee.ai/chatflow/chatbot/v2.0/1d1e0598-3c44-4a8e-bd41-908a9c14846a/message.do";
-
-            var options = {
-                headers : danbee_head,
-                data : JSON.stringify(danbee_body),
-                method : "POST",
-                url : danbee_url
-            }
-           
-            
-            axios(options).then((res) =>{
-
-                
-                var preLog = this.state.log;
-                
-                var cnt = this.state.count + 1;
-                var replyMsg= res.data.responseSet.result.result[0].message;
-                
-                var reply;
-                var send;
-                
-                reply = {
-                    _id: cnt,
-                    text: replyMsg,
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg',
-                        
-                    }
-                }
-                preLog = [messages[0],...preLog];
-                preLog = [reply,...preLog];
-                
-                
-                if(res.data.responseSet.result.another_result[0]){
-                    
-                    for(var i in res.data.responseSet.result.another_result){
-                        
-                        send = {
-                            _id: cnt+10000,
-                            text: <Text 
-                            style={{fontStyle: 'italic', fontWeight: 'bold', textDecorationLine: 'underline'}}
-                            onPress = {(event) => {
-                                
-                                var tmp = [];
-                                var rp = {
-                                    _id: cnt+20000,
-                                    text: event._dispatchInstances.memoizedProps.children,
-                                    createdAt: new Date(),
-                                    user: {
-                                        _id: 1,
-                                        name: 'React Native',
-                                        avatar: 'https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg',
-                                        
-                                    }
-                                }
-                                cnt++;
-                                tmp = [...tmp,rp];
-                        onSend(tmp);
-                            }
-                            }>{res.data.responseSet.result.another_result[i].intent_name}</Text>,
-                            createdAt: new Date(),
-                            user: {
-                                _id: 2,
-                                name: 'React Native',
-                                avatar: 'https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg',
-                                
-                            }
-                        }
-                        cnt++;
-                        preLog = [send,...preLog];
-                    }
-                }
-                
-               
-                this.setState({
-                    log : preLog,
-                    msg : messages[0],
-                    count : cnt
-                });
-            });      
-        }
-
-        
-        return (
-            
-            <View style={{ flex: 1}}>
-                <View style={{alignItems: 'center', justifyContent: 'center',position:'absolute',width:width, top:50,zIndex:10 }}>
-                    <Text style={{ fontSize: 30, fontWeight: "900" }}></Text>
-                </View>
-                <GiftedChat
-                    placeholder={'메세지를 입력하세요...'}
-                    alwaysShowSend={true}
-                    messages={this.state.log}
-                    renderBubble = {this.renderBubble}
-                    textInputProps={{ keyboardAppearance: 'dark', autoCorrect: false }}
-                    onSend={messages => onSend(messages)}
-                    
-                    user={{
+        if (res.data.responseSet.result.another_result[0]) {
+          for (var i in res.data.responseSet.result.another_result) {
+            send = {
+              _id: cnt + 10000,
+              text: (
+                <Text
+                  style={{
+                    fontStyle: "italic",
+                    fontWeight: "bold",
+                    textDecorationLine: "underline",
+                  }}
+                  onPress={(event) => {
+                    var tmp = [];
+                    var rp = {
+                      _id: cnt + 20000,
+                      text: event._dispatchInstances.memoizedProps.children,
+                      createdAt: new Date(),
+                      user: {
                         _id: 1,
-                    }}
+                        name: "React Native",
+                        avatar:
+                          "https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg",
+                      },
+                    };
+                    cnt++;
+                    tmp = [...tmp, rp];
+                    onSend(tmp);
+                  }}
+                >
+                  {res.data.responseSet.result.another_result[i].intent_name}
+                </Text>
+              ),
+              createdAt: new Date(),
+              user: {
+                _id: 2,
+                name: "React Native",
+                avatar:
+                  "https://blog.kakaocdn.net/dn/esugIB/btqwv7PLOZT/oMb10ItMqbhdX0dKomtDQ0/img.jpg",
+              },
+            };
+            cnt++;
+            preLog = [send, ...preLog];
+          }
+        }
 
-                />
-            </View>
-        )
-    }
-    
-  
+        this.setState({
+          log: preLog,
+          msg: messages[0],
+          count: cnt,
+        });
+      });
+    };
+
+    return (
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+            width: width,
+            top: 50,
+            zIndex: 10,
+          }}
+        >
+          <Text style={{ fontSize: 30, fontWeight: "900" }}></Text>
+        </View>
+        <GiftedChat
+          placeholder={"메세지를 입력하세요..."}
+          alwaysShowSend={true}
+          messages={this.state.log}
+          renderBubble={this.renderBubble}
+          textInputProps={{ keyboardAppearance: "dark", autoCorrect: false }}
+          onSend={(messages) => onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 export default ChatScreen;
